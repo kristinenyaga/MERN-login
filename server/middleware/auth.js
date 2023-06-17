423d2cddbcbb5c0b2c548c9c552701518a80fc23
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-17 16:28:23 +0300

    create a file to authorize the user

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+import env from "../config.js";
+import jwt from "jsonwebtoken";
+export default async function Auth(req, res, next) {
+  try {
+    // access authorize header to validate request
+      const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+        // retrieve the user details fo the logged in user
+    const user = await jwt.verify(token, env.JWT_SECRET)
+    req.user = user
+    next()
+  }
+  catch (error) {
+    res.status(401).json({error:"you are not authorized"})
+  }
+}
