6bbea725edb6b68319ea829a9c58713c9d48c982
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-19 23:05:06 +0300

    implement the update profile functionality

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -3,30 +3,42 @@ import profile from '../assets/profile.png'
 import {Link} from 'react-router-dom'
 import styles from '../styles/username.css'
 import { useFormik } from 'formik'
-import { Toaster } from 'react-hot-toast'
+import { Toaster, toast } from 'react-hot-toast'
 import { profileValidate } from '../helper/validate'
 import convert from '..//helper/convert'
+import { useAuthStore } from '../store/store'
+import useFetch from '../hooks/fetch.hook'
+import { updateUser } from '../helper/helper'
+import { useNavigate } from 'react-router-dom'
 const Profile = () => {
-  const [file,setFile] = useState()
+  const [file, setFile] = useState()
+  const nav = useNavigate()
+    const [{isLoading, apiData, status, serverError}]=useFetch()
     const formik = useFormik({
     // specify initial values
       initialValues: {
-        email: "",
-        username: "",
-        password: "",
-        mobile: "",
-        firstname: "",
-        lastname:""
+        email: apiData?.user.email || '',
+        username: apiData?.user.username || '',
+        mobile: apiData?.user.mobile || '',
+        firstName: apiData?.user.firstName || '',
+        lastName: apiData?.user.lastName || '',
+        address:apiData?.user.address || ''
       
-    },
+      },
+      enableReinitialize:true,
     validate:profileValidate,
     // when you want to validate onsubmit
     validateOnBlur: false,
     validateOnChange: false,
       onSubmit: async values => {
         // object.assign copies values from a source to a target object
-      values = await Object.assign(values,{profile:file || ''})
-      console.log(values)
+      values = await Object.assign(values,{profile:file ||apiData?.user.profile || ''})
+        await updateUser(values)
+            .then(() => {
+            toast.success("Updated successfully")
+          })
+          .catch(err => console.log(err))
+        
     }
     })
   // formik doesn't support file upload
@@ -36,6 +48,12 @@ const Profile = () => {
     setFile(base64)
     
   }
+  function userLogout(){
+    localStorage.removeItem('token');
+    nav('/')
+  }
+  if (isLoading) return <h1 className='text-2xl font bold text-center'>isloading</h1>
+  if (serverError) return <h1 className='text-red-500 text-2xl'>{ serverError.message}</h1>
   return (
     <div className='container mx-auto'>
       <Toaster position='top-center' reverseOrder='false'></Toaster>
@@ -49,15 +67,15 @@ const Profile = () => {
           <form onSubmit={formik.handleSubmit}>
             <div className='flex justify-center py-4'>
               <label htmlFor='profile'>
-                <img src={file || profile} alt='profile' className='border-4 border-gray-100 w-[170px]  rounded-md shadow-lg cursor-pointer' />
+                <img src={ apiData?.user.profile || file || profile} alt='profile' className='border-4 border-gray-100 w-[170px]  rounded-md shadow-lg cursor-pointer' />
               </label>
               <input type='file' id='profile' name='profile' onChange={upload}  />
 
             </div>
             <div className='flex flex-col items-center gap-6'>
               <div className='flex gap-2'>
-                 <input type='text' placeholder='firstname' className='shadow-md border-0 py-4  text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('firstname')} />
-                 <input type='text' placeholder='lastname' className='shadow-md border-0 py-4 text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('lastname')} />
+                 <input type='text' placeholder='firstname' className='shadow-md border-0 py-4  text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('firstName')} />
+                 <input type='text' placeholder='lastname' className='shadow-md border-0 py-4 text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('lastName')} />
               </div>
               <div className='flex gap-2'>
                  <input type='number' placeholder='mobile' className='shadow-md border-0 py-4  text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('mobile')} />
@@ -68,7 +86,7 @@ const Profile = () => {
                 <button className='border bg-indigo-600 w-[15em] py-3 rounded-lg text-gray-50 text-xl shadow-md text-center hover:bg-indigo-700'>Register</button>
             </div>
             <div className='text-center py-4'>
-              <span className='text-md'>Come back later? <Link to='/password' className='text-red-500 underline underline-offset-4'>Log Out</Link></span>
+              <span className='text-md'>Come back later? <Link onClick={userLogout} className='text-red-500 underline underline-offset-4'>Log Out</Link></span>
 
             </div>
           </form>
