cd7108c8d48df0b3a0bd3f6b96ca39aea1ccd3f6
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-19 23:08:04 +0300

    implement the password functionality

--- client/src/components/password.jsx
+++ client/src/components/password.jsx
@@ -3,11 +3,17 @@ import profile from '../assets/profile.png'
 import {Link} from 'react-router-dom'
 import styles from '../styles/username.css'
 import { useFormik } from 'formik'
-import { Toaster } from 'react-hot-toast'
+import toast,{ Toaster } from 'react-hot-toast'
 import { passwordValidate } from '../helper/validate'
-
+import { useAuthStore } from '../store/store'
+// import useFetch from '../hooks/fetch.hook'
+import { login } from '../helper/helper'
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../hooks/fetch.hook'
 const Password = () => {
-
+  const nav = useNavigate()
+  const { username } = useAuthStore(state => state.auth)
+  const [{ isLoading, apiData, serverError }] = useFetch(`user/${username}`)
   const formik = useFormik({
     // specify initial values
     initialValues: {
@@ -19,21 +25,31 @@ const Password = () => {
     validateOnChange: false,
     onSubmit: async values => {
       console.log(values)
+      await login({ username, password:values.password })
+        .then((res) => {
+          toast.success("Login was successful")
+          let { token } = res.data
+          localStorage.setItem('token',token)
+          nav('/profile')
+        })
+      .catch(err => toast.error(err.error))
     }
   })
+  if (isLoading) return <h1 className='text-2xl font bold text-center'>isloading</h1>
+  if (serverError) return <h1 className='text-red-500 text-2xl'>{ serverError.message}</h1>
   return (
     <div className='container mx-auto'>
       <Toaster position='top-center' reverseOrder='false'></Toaster>
       <div className='flex justify-center items-center h-screen'>
         <div className= 'glass'>
           <div className='flex flex-col items-center'>
-            <h1 className='text-4xl font-bold'>Hello Again!</h1>
+            <h1 className='text-4xl font-bold'>Hello {apiData?.user.firstName || apiData?.user.username || 'Again!'}</h1>
             <p className='text-sm max-w-[1/4] py-4 font-medium'>Explore more by connecting with us</p>
           </div>
 
           <form onSubmit={formik.handleSubmit}>
             <div className='flex justify-center py-4'>
-              <img src={profile} alt='profile' className='border-4 border-gray-100 w-[150px] rounded-full shadow-lg cursor-pointer' />
+              <img src={apiData?.user.profile || profile} alt='profile' className='border-4 border-gray-100 w-[150px] rounded-full shadow-lg cursor-pointer' />
             </div>
             <div className='flex flex-col items-center gap-6'>
               <input type='password' placeholder='password' className='shadow-md border-0 py-5 px-4 text-start rounded-lg text-md text-gray-700 focus:outline-none' {...formik.getFieldProps('password')} />
